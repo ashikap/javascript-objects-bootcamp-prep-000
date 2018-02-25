d05da34476fbcd53bc5b3c9c8723bfd95f7e298a
ashikap89@gmail.com <ashikap89@gmail.com>
2018-02-25 19:45:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = new Object({keys: '0'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    return artistName = songTitle
+    return [artistName] = songTitle;
+    return playlist;
 }
